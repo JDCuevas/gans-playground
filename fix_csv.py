8bae111dbbf648437acb167ddaddb69724a71986
JDCuevas <julian.cuevas1@upr.edu>
2019-07-01 16:56:38 -0400

    Add files via upload

--- fix_csv.py
+++ fix_csv.py
@@ -9,10 +9,17 @@ for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
 
-    with open(path_in_str, mode='w') as csv_file:
+    with open(path_in_str + '_fixed', mode='w') as fixed_csv_file:
+        fixed_writer = csv.writer(fixed_csv_file, delimiter=',')
+
         with open(path_in_str, mode='r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             next(csv_reader)
             
+            print(path_in_str)
             for row in csv_reader:
+                fixed_r = row['generated'].strip('[]').strip().split(' ')
+                fixed_r = [float(n) for n in fixed_r if n != '']
+                
+                fixed_writer.writerow(fixed_r)
                 
